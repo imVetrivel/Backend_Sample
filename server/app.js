5c99e025db2fcc192579836e75b1f4c654227a7c
imVetrivel <vetrivel2002.k@gmail.com>
2024-09-24 16:16:42 +0530

    msg

--- server/app.js
+++ server/app.js
@@ -1,18 +1,19 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
-const dbcon = require('./config/db')
-
-require('dotenv').config()
+// const dbConn = require('./config/db')
+const Projects = require('./routes/ProjectRoute')
+app.use(express.json())
 
 // const dotenv = require('dotenv')
 // dotenv.config()
 
-const port = process.env.PORT || 7770
-
-app.get('/',(req,res) => {
-    res.json({message:"Welcome"})
+const port = process.env.PORT || 7778
+app.use('/projects', Projects)
+app.get('/', (req, res) => {
+    res.status(400).json( "Welcome")
 })
 
-app.listen(port,() => {
-    console.log(`Server running on port : ${port}`);
+app.listen(port, () => {
+    console.log(`Server running in : ${port}`)
 })
